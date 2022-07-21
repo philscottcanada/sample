4d341d37327d7c74f659eebb6417b761570e85b6
Phil Scott <philscottcanada@gmail.com>
2022-07-21 16:49:36 -0400

    new approach with step by step on the cloudbuild yaml file and incling the app.yaml file in project folder

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>I am Deployed</h1>
+        <h1>I am Deployed #2</h1>
       </header>
     </div>
   );
